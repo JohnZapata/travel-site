910098a53d7028b484a6b1061fc99df173a70d37
JohnZapata <sohrab.m23@gmail.com>
2020-04-03 20:33:55 +0430

    compeleted header section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,13 @@
 import '../styles/styles.css';
+import MobileMenu from "./modules/MobileMenu";
 
-if(module.hot())
+if(module.hot)
    {
    module.hot.accept();
    }
+
+   /* ***************************************************************** */
+
+   
+let mobileMenu = new MobileMenu();
+
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,33 @@
+
+/*
+function Person(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+    this.greet = function() {
+      console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+    }
+  }
+*/
+
+// write Person function constructor with class syntax
+
+class MobileMenu {
+  constructor() {
+    this.menuIcon = document.querySelector(".site-header__menu-icon")
+    this.menuContent = document.querySelector(".site-header__menu-content")
+    this.siteHeader = document.querySelector(".site-header")
+    this.events()
+  }
+
+  events() {
+    this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+  }
+
+  toggleTheMenu() {
+    this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+    this.siteHeader.classList.toggle("site-header--is-expanded")
+    this.menuIcon.classList.toggle("site-header__menu-icon--close-x")
+  }
+}
+
+export default MobileMenu
